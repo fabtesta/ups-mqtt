b0c97d19deeabb0cf9bdc1b0e8f1b14fc25e3ebe
Fabio Testa <fabio.testa@cchellenic.com>
2024-08-09 17:25:04 +0200

    optional 1 time run add requirements.txt

--- ups-mqtt.py
+++ ups-mqtt.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 # ups-mqtt.py
 
 import os
@@ -15,7 +14,7 @@ if not os.path.exists('conf/config.ini'):
 
 # Load configuration file
 config_dir = os.path.join(os.getcwd(), 'conf/config.ini')
-config = ConfigParser(delimiters=('=', ), inline_comment_prefixes=('#'))
+config = ConfigParser(delimiters=('=',), inline_comment_prefixes=('#'))
 config.optionxform = str
 config.read(config_dir)
 
@@ -24,15 +23,17 @@ base_topic = config['MQTT'].get('base_topic', 'home/ups')
 if not base_topic.endswith('/'):
     base_topic += '/'
 
+ups_name = config['UPS'].get('name', 'ups')
 ups_host = config['UPS'].get('hostname', 'localhost')
 mqtt_host = config['MQTT'].get('hostname', 'localhost')
 mqtt_port = config['MQTT'].getint('port', 1883)
 mqtt_user = config['MQTT'].get('username', None)
 mqtt_password = config['MQTT'].get('password', None)
-interval = config['General'].getint('interval', 60)
+interval = config['GENERAL'].getint('interval', 60)
+
 
 def process():
-    ups = subprocess.run(["upsc", "ups@" + ups_host], stdout=subprocess.PIPE)
+    ups = subprocess.run(["upsc", ups_name + "@" + ups_host], stdout=subprocess.PIPE)
     lines = ups.stdout.decode('utf-8').split('\n')
 
     msgs = []
@@ -49,15 +50,18 @@ def process():
             cached_values[key] = value
             topic = base_topic + key.replace('.', '/').replace(' ', '_')
             msgs.append((topic, value, 0, True))
-        
+
         timestamp = time.time()
         msgs.append((base_topic + 'timestamp', timestamp, 0, True))
-        msgs.append((base_topic + 'lastUpdate', datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S %Z'), 0, True))
+        msgs.append((base_topic + 'lastUpdate',
+                     datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S %Z'), 0, True))
         mqtt.multiple(msgs, hostname=mqtt_host, port=mqtt_port, auth={'username': mqtt_user, 'password': mqtt_password})
 
 
-
-while True:
-    process()
-    sleep(interval)
-    
+if __name__ == '__main__':
+    while True:
+        process()
+        if interval > 0:
+            sleep(interval)
+        else:
+            break
